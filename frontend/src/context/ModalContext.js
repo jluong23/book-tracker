e166226815e8a3827329f91522db36fc9cdf55aa
James Luong <jamesluong@hotmail.co.uk>
2022-09-21 14:50:51 +0100

    Closed modal when backdrop is cilcked

--- frontend/src/context/ModalContext.js
+++ frontend/src/context/ModalContext.js
@@ -25,11 +25,17 @@ export const ModalContextProvider = ({ children }) => {
   const closeModal = () => {
     dispatch({type: 'CLOSE'});
   }
+
+  const backgroundClicked = (e) => {
+    if(e.target.id === "modal-background"){
+      closeModal();
+    }
+  }
   
   return (
     <ModalContext.Provider value={{ ...state, dispatch }}>
       {state.visible && 
-        <div className="fixed inset-0 bg-opacity-30 bg-black backdrop-blur-sm flex justify-center items-center">
+        <div onClick={backgroundClicked} id="modal-background" className="fixed inset-0 bg-opacity-30 bg-black backdrop-blur-sm flex justify-center items-center">
           {/* Modal */}
           <div className="bg-white p-1 rounded">
               <AiFillCloseCircle className="right-0 float-right relative cursor-pointer text-lg" onClick={closeModal}/>
